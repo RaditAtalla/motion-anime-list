8ac06a5566947bcb06b4f455770f23643fb264d7
RaditAtalla <radityaderachmadie@gmail.com>
2026-01-10 00:22:12 +0700

    show favorite anime fetched from local database

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:motion_anime_list/components/anime_card.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:motion_anime_list/models/anime_model.dart';
+import 'package:motion_anime_list/widgets/anime_card.dart';
 
-class FavoriteScreen extends StatelessWidget {
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  late List<Anime> favoriteAnimeList = [];
+
+  void fetchfavoriteAnime() {
+    var favoriteBox = Hive.box('fav-anime');
+
+    for (var anime in favoriteBox.values) {
+      favoriteAnimeList.add(anime);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchfavoriteAnime();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +39,22 @@ class FavoriteScreen extends StatelessWidget {
           scrolledUnderElevation: 0,
         ),
 
-        // body: Padding(
-        //   padding: const EdgeInsets.only(top: 50),
-        //   child: GridView.builder(
-        //     padding: EdgeInsets.all(20),
-        //     itemCount: 10,
-        //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        //       mainAxisExtent: 500,
-        //       crossAxisCount: 2,
-        //       crossAxisSpacing: 10,
-        //     ),
-        //     itemBuilder: (context, index) {
-        //       return AnimeCard();
-        //     },
-        //   ),
-        // ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: GridView.builder(
+            padding: EdgeInsets.all(20),
+            itemCount: favoriteAnimeList.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisExtent: 500,
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) {
+              var anime = favoriteAnimeList[index];
+              return AnimeCard(anime: anime);
+            },
+          ),
+        ),
       ),
     );
   }
